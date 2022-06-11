2f6198cff8ea3ab407a0eb37953ebb38da09a2f8
jherden84 <88911662+jherden84@users.noreply.github.com>
2022-06-12 01:17:21 +1000

    access6

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -44,13 +44,6 @@ export class MelviewMitsubishiPlatformAccessory {
           }
         };
 
-        //Create a Siwtch for Zones
-        this.acService = new this.Service(this.Service.Switch);
-     // create handlers for required characteristics
-     this.acService.getCharacteristic(this.Characteristic.On)
-       .onGet(this.handleOnGet.bind(this))
-       .onSet(this.handleOnSet.bind(this));
-
 
         /*********************************************************
          * Polling for state change
@@ -70,15 +63,50 @@ export class MelviewMitsubishiPlatformAccessory {
             });
         }, 5000);
     }
+  }
 
-    handleOnGet() {
-       this.platform.log.info('Zone Triggered GET On');
-       // set this to a valid value for On
-         const currentValue = 1;
-         return currentValue;
-       }
 
-   handleOnSet(value) {
-           this.platform.log.info('Zone Triggered SET On');
+  class ExampleSwitchAccessory {
+
+    constructor(log, config, api) {
+        this.log = log;
+        this.config = config;
+        this.api = api;
+
+        this.Service = this.api.hap.Service;
+        this.Characteristic = this.api.hap.Characteristic;
+
+        // extract name from config
+        this.name = config.name;
+
+        // create a new Switch service
+        this.service = new this.Service(this.Service.Switch);
+
+        // create handlers for required characteristics
+        this.service.getCharacteristic(this.Characteristic.On)
+          .onGet(this.handleOnGet.bind(this))
+          .onSet(this.handleOnSet.bind(this));
+
     }
+
+    /**
+     * Handle requests to get the current value of the "On" characteristic
+     */
+    handleOnGet() {
+      this.log.debug('Triggered GET On');
+
+      // set this to a valid value for On
+      const currentValue = 1;
+
+      return currentValue;
+    }
+
+    /**
+     * Handle requests to set the "On" characteristic
+     */
+    handleOnSet(value) {
+      this.log.debug('Triggered SET On:' value);
+    }
+
+  }
 }
