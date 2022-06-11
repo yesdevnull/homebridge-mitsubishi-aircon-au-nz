6562a56a1741c0efbffb2544caa1a3754184becb
jherden84 <88911662+jherden84@users.noreply.github.com>
2022-06-11 19:04:48 +1000

    folk2

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'MitsubishiAUNZ_jph';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-airconditioner-mitsubishi-au-nz';
+export const PLUGIN_NAME = 'homebridge-airconditioner-mitsubishi-au-nz-jph';
