3dd37f3ee2e89fbbfc871708c7fc5dc54aafe698
jherden84 <88911662+jherden84@users.noreply.github.com>
2022-06-11 23:23:51 +1000

    rename

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'MitsubishiAUNZ_jph';
+export const PLATFORM_NAME = 'MitsubishiAUNZ';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-airconditioner-mitsubishi-au-nz-jph';
+export const PLUGIN_NAME = 'homebridge-airconditioner-mitsubishi-au-nz';
